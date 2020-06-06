00710efd686f1ad5761fa91626aee29e3ac2c58f
Ka Cheng Ieong <ieongkacheng@gmail.com>
2020-06-06 14:00:39 -0400

    Select a color from dropdown menu of colors

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Select a color from dropdown menu of colors
 class App extends Component {
-  
-
+  constructor() {
+    super(props);
+    this.state = {color: ""};
+  }
+  render() {
+    return (
+      <select value={this.state.color} onChange={this.handleChange}>        
+        <option value="red">Red</option>
+        <option value="blue">Blue</option>
+        <option value="gray">Gray</option>
+        <option value="green">Green</option>
+        <option value="yellow">Yellow</option>
+        <option value="orange">Orange</option>
+      </select>
+    );
+  }
 }
 
 export default App;
