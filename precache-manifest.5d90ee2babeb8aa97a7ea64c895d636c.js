18ca8457cdd7f782b82b08550ea0c2776b4b41be
Ka Cheng Ieong <ieongkacheng@gmail.com>
2020-06-07 18:13:08 -0400

    Updates

---
+++ precache-manifest.5d90ee2babeb8aa97a7ea64c895d636c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47bb483684563e416844ba0827e0543",
+    "url": "/Assignment-8/index.html"
+  },
+  {
+    "revision": "b0a8d0a2b10dd96fdc76",
+    "url": "/Assignment-8/static/css/main.972ef2c7.chunk.css"
+  },
+  {
+    "revision": "18491eb22688c0b23bca",
+    "url": "/Assignment-8/static/js/2.7c5d9bdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Assignment-8/static/js/2.7c5d9bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0a8d0a2b10dd96fdc76",
+    "url": "/Assignment-8/static/js/main.9210cb17.chunk.js"
+  },
+  {
+    "revision": "860d956142a8bc231b6d",
+    "url": "/Assignment-8/static/js/runtime-main.7a7cd9f1.js"
+  }
+]);
